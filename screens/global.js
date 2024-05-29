54d283c216516eb1d33200d85473ba5cf7dfb006
Abdul-Mateen11 <145853946+Abdul-Mateen11@users.noreply.github.com>
2024-05-29 14:05:08 +0500

    Final

---
+++ screens/global.js
@@ -0,0 +1,13 @@
+// globals.js
+export let phoneNumber = null;
+export let username = null;
+
+export const setPhoneNumber = (phone) => {
+    phoneNumber = phone;    
+};
+
+export const setnameglobal = (name) => {
+    username = name;
+    console.log('Username Set')
+};
+
